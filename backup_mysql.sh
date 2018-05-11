ca292037e4402e01ec4fe18cf137fd015976159e
gaoyuxia <banan.gao@byd.com>
2018-05-11 19:12:20 +0800

    modified backup_mysql.sh

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #备份数据库
 
-rm -rf reviewdb_project`date +%Y-%m-%d`
-mysqldump -u root -p reviewdb_project > /home/byd/backup_script/reviewdb_project`date +%Y-%m-%d`
+rm -rf newreviewdb_project_`date +%F` reviewdb_project_`date +%F`
+mysqldump -u root -p reviewdb_project > /home/byd/backup_script/reviewdb_project_`date +%F`
+mysqldump -u root -p newreviewdb_project > /home/byd/backup_script/newreviewdb_project_`date +%F`
 
