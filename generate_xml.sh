cad35e2b66bc99021875a901551f332265dd6b82
xia1990 <15871966050@163.com>
2018-06-14 21:59:50 +0800

    Update generate_xml.sh

--- generate_xml.sh
+++ generate_xml.sh
@@ -6,7 +6,7 @@ function generate_xml(){
 	for i in `cat project_name.txt`
 	do
 		#${i#*/}:在此处去掉仓库名称的前缀
-		echo "<project name=\"$i\" path=\"${i#*/}\">" >> default.xml
+		echo "<project name=\"$i\" path=\"${i#*/}\"/>" >> default.xml
 	done
 	#在第一行添加
 	sed -i "1i <manifest>\n<remote fetch=\"ssh://192.168.56.101:29418//\" name=\"origin\" review=\"http://192.168.56.101:8081/\"/>\n<default remote=\"origin\" revision=\"master\"/>" default.xml
