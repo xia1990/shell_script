7f5d0e5439dbfcd1fcce5436fc3bdaf296c32a0b
gaoyuxia <banan.gao@byd.com>
2018-07-02 15:27:34 +0800

    添加收集release_note的脚本

---
+++ get_release_note.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#收集release_note信息
+#此脚本需先使用repo diffmenifests old.xml new.xml,生成diff_file.txt文件
+
+PATHROOT=$(pwd)
+PROJECT="JA32_BSP"
+
+#得到仓库名称，开始commit,结束commit
+grep "changed from" "$1" | awk '{print $1,$4,$6}' > file.txt
+readarray -t file_array < file.txt
+for line in "${file_array[@]}"
+do
+	name=$(echo $line | awk '{print $1}')
+	echo $name
+	begincommit=$(echo $line | awk '{print $2}')
+	echo $begincommit
+	endcommit=$(echo $line | awk '{print $NF}')
+	echo $endcommit
+	pushd ${PATHROOT}/$PROJECT/$name
+	pwd
+		git log --pretty=format:"%H,%an,%s" $begincommit..$endcommit | grep -v "Merged" > release_note.txt
+	popd
+done
+find . -name "release_note.txt" | xargs cat | tee -a release_note
