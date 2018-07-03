ef336acf40766a9ffde5d8e0fcabad2e91f9472a
gaoyuxia <banan.gao@byd.com>
2018-07-03 18:06:15 +0800

    add ja32_bsp.sh

--- ja32_bsp.sh
+++ ja32_bsp.sh
@@ -16,7 +16,6 @@ function clean_code(){
 		repo init -u ssh://yinjigang@10.30.99.88:29418/JA32_BSP/android/manifest -b ja32_byd_bsp -m default.xml
 		repo sync -j4		
 	fi
-	popd
 }
 
 function build_linux(){
@@ -31,18 +30,16 @@ function build_linux(){
 function build_L4_v2(){
 		pushd ${PATHROOT}/$PROJECT/L4_v2
 			echo "begin build L4_v2"
-			ln -s ${PATHROOT}/$PROJECT/linux/vendor/kyocera/buildscm kcbuild
-			cp ${PATHROOT}/$PROJECT/linux/vendor/kyocera/buildscm/kcc_amss.sh .
+			ln -s ../linux/vendor/kyocera/buildscm kcbuild
+			cp ../linux/vendor/kyocera/buildscm/kcc_amss.sh .
 			export HEXAGON_ROOT=/pkg/qct/software/hexagon/releases/tools
 			
 			cp ${PATHROOT}/kcc_amss_sub.sh ${PATHROOT}/${PROJECT}/L4_v2/kcbuild
 			cp ${PATHROOT}/qfile_archive.xml ${PATHROOT}/${PROJECT}/L4_v2/kcbuild/Deploy
-			
 			mkdir LINUX
 			cd LINUX
 			ln -s ../../linux android
 			cd -
-
 			./kcc_amss.sh -v userdebug 2>&1 | tee -a build_L4_v2.log
 			echo "build L4_v2 END"
 		popd
@@ -50,7 +47,6 @@ function build_L4_v2(){
 
 function make_zipfile(){
 	pushd ${PATHROOT}/$PROJECT/L4_v2
-		echo "begin packing"
 		./kcc_amss.sh --rom common
 
 		pushd deploy_$DATE/bin
@@ -59,7 +55,7 @@ function make_zipfile(){
 
 		pushd QFILE_FLAT_img_$DATE
 			pushd bin
-				zip -r -9 JA32_BSP_$DATE.zip ./*
+				zip -r -9 JA32_BSP_$VERSION_$DATE.zip ./*
 			popd
 		popd
 	popd
@@ -78,14 +74,18 @@ ftp -n 10.30.11.100 2>&1 <<EOC
   cd target
   lcd ${PATHROOT}/$PROJECT/L4_v2/QFILE_FLAT_img_$DATE/bin
   put JA32_BSP_$DATE.zip 
-  lcd ${PATHROOT}/$PROJECT/L4_v2/deploy_$DATE/bin
+  lcd ${PATHROOT}/$PROJECT/L4_v2/deploy_$DATE
   put deploy_$DATE.zip
   bye
 EOC
 }
 ########################
-#clean_code
-#build_linux
-#build_L4_v2
-#make_zipfile
-ftp_upload
+if [ $# -eq 1 ];then
+	clean_code
+	build_linux
+	build_L4_v2
+	make_zipfile
+	ftp_upload
+else
+	echo "请输入版本号"
+fi
