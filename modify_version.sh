b9d9f6c936a5984805a9d819370fe60ed522d91a
xia1990 <18016074089@163.com>
2020-09-15 16:20:16 +0800

    Add files via upload

--- modify_version.sh
+++ modify_version.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-#修改1401版本号
 
-PATHROOT=$(pwd)
-PROJECT="Wi1401Q_S7AP_DEV"
 
-function change_version_number(){
-	pushd ${PATHROOT}/${PROJECT}/LINUX/android/build/tools
-		Old_VER_NUM=$(grep -i "OEM_PRODUCT_VERSION_SHORT.*:=" "byd_buildinfo.mk" | awk '{print $NF}')
-		echo $Old_VER_NUM
-		TMP_VER_NUM=`expr ${Old_VER_NUM} + 1`
-		echo $TMP_VER_NUM
-		NEW_VER_NUM=`printf %06d ${TMP_VER_NUM}`
-		echo $NEW_VER_NUM
-		sed -i s/OEM_PRODUCT_VERSION_SHORT.*:=.*/OEM_PRODUCT_VERSION_SHORT\ :=\ ${NEW_VER_NUM}/g byd_buildinfo.mk
-	popd
-}
-
-change_version_number
+old_number=$(grep "\$(BUILD_ID_PREFIX)" build_id-bq-borneo.mk | awk -F'-' '{print $2}')
+new_number=$((old_number+1))
+echo $new_number
